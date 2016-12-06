57efbc746c3a5231150303de830a8b72b0d001f5
Connor Abdelnoor <connor.abd@gmail.com>
2016-12-06 10:40:19 -0800

    add jasmine testing library

---
+++ src/Song.js
@@ -0,0 +1,11 @@
+function Song() {
+}
+
+Song.prototype.persistFavoriteStatus = function(value) {
+  // something complicated
+  throw new Error("not yet implemented");
+};
+
+function addTwo(number) {
+    return number + 2;
+}
