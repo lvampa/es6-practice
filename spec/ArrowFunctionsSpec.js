4923a1f1ecb3c21c0ad8019e33d1f3036fd05191
Connor Abdelnoor <connor.abd@gmail.com>
2016-12-12 10:21:46 -0800

    refactor

--- spec/ArrowFunctionsSpec.js
+++ spec/ArrowFunctionsSpec.js
@@ -1,12 +1,8 @@
-describe("ES6 Arrow Function Refactoring", function() {
+describe("ES6 Arrow Functions", function() {
 
-    describe("It should let you write less code", function() {
+    describe("Simple old school functions", function() {
         it("it should add two numbers together", function() {
            expect(5).toEqual(addTogether(3,2));
-        });
-
-        it("It should add two numbers together in ES6", function() {
-           expect(7).toEqual(addTogetherArrow(5,2));
         });
 
         it("It should add 3 to each element of the array", function() {
@@ -15,21 +11,30 @@ describe("ES6 Arrow Function Refactoring", function() {
             expect(b).toEqual(addThreeToEach(a));
         });
 
+        it("It should say 'Hello'", function() {
+            expect("Hello").toEqual(sayHello());
+        });
+
+    });
+
+    describe("Simple functions refactored with ES6 arrow functions", function() {
+        it("It should add two numbers together in ES6", function() {
+           expect(7).toEqual(addTogetherArrow(5,2));
+        });
+
         it("It should add 3 to each element of the array in ES6", function() {
             let a = [2, 3, 4];
             let b = [5, 6, 7];
             expect(b).toEqual(addThreeToEachArrow(a));
         })
 
-        it("It should say 'Hello'", function() {
-            expect("Hello").toEqual(sayHello());
-        });
-
         it("It should say 'Howdy' in ES6", function() {
             expect("Howdy").toEqual(sayHelloArrow());
         });
-    });
+    })
 
-    
+    describe("", function() {
+        
+    });
 
 })
